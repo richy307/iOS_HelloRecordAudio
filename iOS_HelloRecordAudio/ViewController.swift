2499eefceab3ef7b8909f2128e0de4a3587d271e
Richy <richy@webcomm.com.tw>
2023-07-20 14:17:20 +0800

    設定 AudioSession 音訊工作階段, 實作 AVAudioRecorderDelegate, recordAudio, stopRecording

--- iOS_HelloRecordAudio/ViewController.swift
+++ iOS_HelloRecordAudio/ViewController.swift
@@ -8,38 +8,85 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+enum AudioSessionMode {
+    case record
+    case play
+}
+
+class ViewController: UIViewController, AVAudioRecorderDelegate {
     
     var audioRecorder: AVAudioRecorder?
     var audioPlayer: AVAudioPlayer?
     var isRecording = false
 
     @IBAction func recordAudio(_ sender: UIButton) {
+        settingAudioSession(toMode: .record)
+        audioRecorder?.prepareToRecord()
+        audioRecorder?.record() // 開始錄音
+        isRecording = true
     }
+    
     @IBAction func stopRecording(_ sender: UIButton) {
+        audioRecorder?.stop() // 停止錄音
+        isRecording = false
+        settingAudioSession(toMode: .play)
     }
+    
     @IBAction func playRecordedSound(_ sender: UIButton) {
     }
+    
     @IBAction func stopPlaying(_ sender: UIButton) {
     }
     
+    // 音訊工作階段
+    func settingAudioSession(toMode mode: AudioSessionMode){
+        let session = AVAudioSession.sharedInstance()
+        
+        do {
+            switch mode {
+            case .record:
+                try session.setCategory(.playAndRecord, mode: .default, options: [])
+            case .play:
+                try session.setCategory(.playback, mode: .default, options: [])
+            }
+            try session.setActive(false)
+            
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    // 實作 AVAudioRecorderDelegate func
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag == true {
+            
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: recorder.url)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         // init an audio recorder
-        let filename = "User.wav"
-        let path = NSHomeDirectory() + "/Documents/" + filename
+        let filename = "User.wav" // 檔名
+        let path = NSHomeDirectory() + "/Documents/" + filename // 儲存路徑
         let url = URL(fileURLWithPath: path)
+        // 錄音設定
         let recordSettings: [String:Any] = [
             AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue,
             AVEncoderBitRateKey: 16,
             AVNumberOfChannelsKey: 2,
             AVSampleRateKey: 44100.0
         ]
+        
         do {
             audioRecorder = try AVAudioRecorder(url: url, settings: recordSettings)
+            audioRecorder?.delegate = self // 實作介面
         } catch {
             print(error.localizedDescription)
         }
